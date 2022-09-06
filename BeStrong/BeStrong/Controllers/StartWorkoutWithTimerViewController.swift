549b36647fdc4bc824bdd27c3785c05942cfed55
AndyPolDev <AndyPolDev@gmail.com>
2022-09-06 18:32:20 +0300

    BeStrong-005. Create StartWorkoutVC’s

---
+++ BeStrong/BeStrong/Controllers/StartWorkoutWithTimerViewController.swift
@@ -0,0 +1,115 @@
+import UIKit
+
+class StartWorkoutWithTimerViewController: UIViewController {
+    
+    private let startWorkoutLabel = UILabel(text: "START WORKOUT", font: .robotoMedium24(), textColor: .specialGray)
+    
+    private lazy var closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(UIImage(named: "closeButton"), for: .normal)
+        //
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(closeButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let ladyWorkoutImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "timer")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let detailsLabel = UILabel(text: "Details")
+    
+    private let detailsView = DetailsWithTimerView()
+    
+    private lazy var finishButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .specialGreen
+        button.layer.cornerRadius = 10
+        button.setTitle("FINISH", for: .normal)
+        button.tintColor = .white
+        button.titleLabel?.font = .robotoMedium16()
+        button.addTarget(self, action: #selector(finishButtonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setViews()
+        setConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        closeButton.layer.cornerRadius = closeButton.frame.width / 2
+    }
+    
+    private func setViews() {
+        view.backgroundColor = .specialBackground
+        
+        view.addSubview(startWorkoutLabel)
+        view.addSubview(closeButton)
+        view.addSubview(ladyWorkoutImageView)
+        view.addSubview(detailsLabel)
+        view.addSubview(detailsView)
+        view.addSubview(finishButton)
+    }
+    
+    @objc private func closeButtonPressed() {
+        print("Close button pressed")
+    }
+    
+    @objc private func finishButtonPressed() {
+        print("Finish button pressed")
+    }
+}
+
+//MARK: - Set Constraints
+
+extension StartWorkoutWithTimerViewController {
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            startWorkoutLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            startWorkoutLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            closeButton.centerYAnchor.constraint(equalTo: startWorkoutLabel.centerYAnchor),
+            closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            closeButton.heightAnchor.constraint(equalToConstant: 30),
+            closeButton.widthAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        NSLayoutConstraint.activate([
+            ladyWorkoutImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            ladyWorkoutImageView.topAnchor.constraint(equalTo: startWorkoutLabel.bottomAnchor, constant: 25),
+            ladyWorkoutImageView.widthAnchor.constraint(equalToConstant: 250),
+            ladyWorkoutImageView.heightAnchor.constraint(equalToConstant: 250)
+        ])
+
+        NSLayoutConstraint.activate([
+            detailsLabel.topAnchor.constraint(equalTo: ladyWorkoutImageView.bottomAnchor, constant: 25),
+            detailsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            detailsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25)
+        ])
+        
+        NSLayoutConstraint.activate([
+            detailsView.topAnchor.constraint(equalTo: detailsLabel.bottomAnchor, constant: 3),
+            detailsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            detailsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            detailsView.heightAnchor.constraint(equalToConstant: 235)
+        ])
+    
+        NSLayoutConstraint.activate([
+            finishButton.topAnchor.constraint(equalTo: detailsView.bottomAnchor, constant: 25),
+            finishButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            finishButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            finishButton.heightAnchor.constraint(equalToConstant: 55)
+        ])
+    }
+}
---
+++ BeStrong/BeStrong/Views/StartWorkoutWithTimerVC/DetailsWithTimerView.swift
@@ -0,0 +1,153 @@
+import UIKit
+
+class DetailsWithTimerView: UIView {
+    
+    private let workoutName = UILabel(text: "Squats", font: .robotoMedium24(), textColor: .specialGray)
+    
+    private let setsLabel = UILabel(text: "Sets", font: .robotoMedium18(), textColor: .specialGray)
+    
+    private let setsProgressLabel = UILabel(text: "1/4", font: .robotoMedium24(), textColor: .specialGray)
+    
+    private let firstSeparatingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .specialLightBrown
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let timerLabel = UILabel(text: "Time of Set", font: .robotoMedium18(), textColor: .specialGray)
+    
+    private let timerProgressLabel = UILabel(text: "1 min 30 sec", font: .robotoMedium24(), textColor: .specialGray)
+    
+    private let secondSeparatingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .specialLightBrown
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var editingButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .clear
+        button.setTitle("Editing", for: .normal)
+        button.tintColor = .specialLightBrown
+        button.titleLabel?.font = .robotoMedium16()
+        button.setImage(UIImage(named: "pencil"), for: .normal)
+        button.addTarget(self, action: #selector(editingButtonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var nextSetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .specialYellow
+        button.layer.cornerRadius = 10
+        button.setTitle("NEXT SET", for: .normal)
+        button.tintColor = .specialGray
+        button.titleLabel?.font = .robotoMedium16()
+        button.addTarget(self, action: #selector(nextSetButtonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    private var firstLineStackView = UIStackView()
+    private var secondLineStackView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        backgroundColor = .specialBrown
+        layer.cornerRadius = 10
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(workoutName)
+        firstLineStackView = UIStackView(arrangedSubviews: [setsLabel,
+                                                            setsProgressLabel],
+                                         axis: .horizontal,
+                                         spacing: 10)
+        firstLineStackView.distribution = .equalSpacing
+        addSubview(firstLineStackView)
+        addSubview(firstSeparatingView)
+    
+        secondLineStackView = UIStackView(arrangedSubviews: [timerLabel,
+                                                             timerProgressLabel],
+                                          axis: .horizontal,
+                                          spacing: 10)
+        secondLineStackView.distribution = .equalSpacing
+        addSubview(secondLineStackView)
+        addSubview(secondSeparatingView)
+        addSubview(editingButton)
+        addSubview(nextSetButton)
+    }
+    
+    @objc private func editingButtonPressed() {
+        print("EditingButtonPressed")
+    }
+    
+    
+    @objc private func nextSetButtonPressed() {
+        print("NextSetButtonPressed")
+    }
+}
+
+//MARK: - Set Constraints
+
+extension DetailsWithTimerView {
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            workoutName.centerXAnchor.constraint(equalTo: centerXAnchor),
+            workoutName.topAnchor.constraint(equalTo: topAnchor, constant: 10)
+        ])
+
+        NSLayoutConstraint.activate([
+            firstLineStackView.topAnchor.constraint(equalTo: workoutName.bottomAnchor, constant: 15),
+            firstLineStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            firstLineStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+        ])
+
+        NSLayoutConstraint.activate([
+            firstSeparatingView.topAnchor.constraint(equalTo: firstLineStackView.bottomAnchor, constant: 3),
+            firstSeparatingView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            firstSeparatingView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            firstSeparatingView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+
+        NSLayoutConstraint.activate([
+            secondLineStackView.topAnchor.constraint(equalTo: firstSeparatingView.bottomAnchor, constant: 15),
+            secondLineStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            secondLineStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+        ])
+
+        NSLayoutConstraint.activate([
+            secondSeparatingView.topAnchor.constraint(equalTo: secondLineStackView.bottomAnchor, constant: 3),
+            secondSeparatingView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            secondSeparatingView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            secondSeparatingView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+        
+        NSLayoutConstraint.activate([
+            editingButton.topAnchor.constraint(equalTo: secondSeparatingView.bottomAnchor, constant: 10),
+            editingButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            editingButton.heightAnchor.constraint(equalToConstant: 25),
+            editingButton.widthAnchor.constraint(equalToConstant: 75)
+        ])
+
+        NSLayoutConstraint.activate([
+            nextSetButton.topAnchor.constraint(equalTo: editingButton.bottomAnchor, constant: 10),
+            nextSetButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+            nextSetButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            nextSetButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15)
+        ])
+    }
+}
